69d961d036c1a2a90c26308e61bf3e65c737ee91
Frank <gyrometor@gmail.com>
2019-04-28 02:03:05 -0400

    add property value editor

--- scripts/taskGUI.js
+++ scripts/taskGUI.js
@@ -35,34 +35,138 @@ var taskGUI = {
 			td.appendChild(part.nameBox);
 			part.row2.appendChild(td);
 			
+			var mTask = task;
+			
+			// CREATE PROPERTY EDITOR
+			
+			part.hideEditor = function()
+			{
+				if(this.hasOwnProperty('editorDiv'))
+				{
+					this.container.removeChild(this.editorDiv);
+					delete this.editorDiv;
+				}
+			}
+			
+			part.applyEditorChanges = function()
+			{
+				if(this.hasOwnProperty('editorDiv'))
+				{
+					var enteredValues = this.editorTextArea.value.split(',');
+					var newValues = [];
+					
+					// ensure empty or null values are removed
+					for(var i=0; i<enteredValues.length; i++)
+					{
+						if(enteredValues[i] != null && enteredValues[i] != '')
+							newValues[newValues.length] = enteredValues[i];
+					}
+					
+					console.log(enteredValues);
+					console.log(newValues);
+					
+					if(newValues.length > 0)
+					{
+						var key = Object.keys(this.task.properties)[this.editorKeyID];
+						this.task.properties[key] = newValues;
+						this.hideEditor();
+					}
+					else
+						alert('No valid values entered!');
+				}
+			}
+			
+			part.showEditor = function(keyID)
+			{
+				this.hideEditor();	// ensure there is not an active editor
+				
+				this.editorKeyID = keyID;
+				this.editorDiv = document.createElement('div');
+				
+				var mKeys = Object.keys(this.task.properties);
+				var mKey = mKeys[keyID];
+				
+				// create HTML elements
+				var table = document.createElement('table');				// table & row
+				var tr = document.createElement('tr');
+				table.appendChild(tr);
+				
+				var td =  document.createElement('td');						// first table data
+				tr.appendChild(td);
+				
+				var label = document.createElement('span');					// property name label
+				label.innerHTML = mKeys[keyID] + ': <br/>';
+				td.appendChild(label);
+				
+				var button = document.createElement('button');				// 'apply' button
+				button.innerHTML = 'Apply';
+				button.onclick = function(){part.applyEditorChanges()};
+				td.appendChild(button);
+				
+				td =  document.createElement('td');							// seond table data
+				tr.appendChild(td);
+				
+				part.editorTextArea = document.createElement('textArea');	// text area
+				part.editorTextArea.rows = 4;
+				part.editorTextArea.cols = 60;
+				td.appendChild(part.editorTextArea);
+				
+				// set textArea value
+				for(var i=0; i<this.task.properties[mKey].length; i++)
+					part.editorTextArea.value += this.task.properties[mKey][i] + ',';
+				
+				this.editorDiv.appendChild(table);
+				this.container.appendChild(this.editorDiv);
+			};
+			
 			// ADD TASK PROPERTIES
 			
 			var keys = Object.keys(task.properties);
 			for(var i=0; i<keys.length; i++)
 			{
-				// create textbox
-				var mBox = document.createElement('input');
-				mBox.type = 'text';
-				mBox.value = keys[i];
+				var mKeyID = i;
 				
 				// create & append td
-				td = document.createElement('td');
-				td.appendChild(mBox);
-				part.row1.appendChild(td);
+				var mLabel = document.createElement('td');
+				mLabel.innerHTML = keys[i];
+				part.row1.appendChild(mLabel);
 				
 				// create buttons
 				var mButton1 = document.createElement('button');
-				mButton1.onclick = function(){alert(mBox.value);};
-				mButton1.innerHTML = 'Edit...';
+				mButton1.onclick = function(){part.showEditor(mKeyID);};
+				mButton1.innerHTML = 'Edit';
 				
 				var mButton2 = document.createElement('button');
-				mButton2.onclick = function(){alert('Delete..?');};
+				mButton2.onclick = function(){alert('delete..?');};
 				mButton2.innerHTML = 'Delete';
+				
+				var mButton3 = document.createElement('button');
+				mButton3.onclick = function(){	// replace the existing key name
+					var mKeys = Object.keys(mTask.properties);
+					var newKey = prompt('Enter a new name for this property: ', mKeys[mKeyID]);
+					
+					// replace the new key if the user did not click 'cancel'
+					if(newKey != null)
+					{
+						// ensure that another key is not overwritten
+						for(var j=0; j<mKeys.length; j++)
+						{
+							if(j != mKeyID && mKeys[j] == newKey)
+								return;
+						}
+						
+						// replace the key name
+						replaceKey(mTask.properties, mKeys[mKeyID], newKey);
+						mLabel.innerHTML = newKey;
+					}
+				};
+				mButton3.innerHTML = 'Rename';
 				
 				// create & append td
 				td = document.createElement('td');
 				td.appendChild(mButton1);
 				td.appendChild(mButton2);
+				td.appendChild(mButton3);
 				part.row2.appendChild(td);
 				
 			}
--- scripts/util.js
+++ scripts/util.js
@@ -3,3 +3,20 @@ function clone(obj)
 {
 	return JSON.parse(JSON.stringify(obj));
 }
+
+// replace a key name in an object, return true if it existed
+function replaceKey(obj, oldKey, newKey)
+{
+	if(!obj.hasOwnProperty(oldKey))
+	{
+		obj[newKey] = null;
+		return false;
+	}
+	else
+	{
+		var value = obj[oldKey];
+		delete obj[oldKey];
+		obj[newKey] = value;
+		return true;
+	}
+}
